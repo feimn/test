1f4610c4dded96dcd1efef58036daf79ff493254
feimn <1803351335@qq.com>
2020-07-23 11:32:43 +0800

    ioc加注释说明  以及流程

--- Ioc.php
+++ Ioc.php
@@ -5,24 +5,22 @@ interface Log
     public function write();
 }
 
-class FileLog implements Log
-{
-    public function write()
-    {
-        // TODO: Implement write() method.
-        echo "filelog write";
-    }
-}
-
-
 class DatabaseLog implements Log
 {
     public function write()
     {
-        // TODO: Implement write() method.
-        echo "databaselog write";
+        echo"这是数据库写入日志的方式";
     }
 }
+
+class FileLog implements Log
+{
+    public function write()
+    {
+       echo"这里是文件记录日志的方式";
+    }
+}
+
 
 class User
 {
@@ -33,9 +31,10 @@ class User
         $this->log = $log;
     }
 
+
     public function login()
     {
-        echo"login success"."<br/>";
+        echo"登录成功"."<br/>";
         $this->log->write();
     }
 }
@@ -44,34 +43,46 @@ class Ioc
 {
     public $binding = [];
 
-
-    public function  bind($abstract,$concrete)
+    public function bind($abstract,$concrete)
     {
         $this->binding[$abstract]['concrete'] = function($ioc) use($concrete){
             return $this->build($concrete);
         };
     }
 
+    // 解析实例的方法
     public function make($abstract)
     {
         $concrete = $this->binding[$abstract]['concrete'];
         return $concrete($this);
     }
 
+    //  处理具体类的方法
+
     public function build($concrete)
     {
+        //  这里用到类的反射机制
 
         $reflector = new ReflectionClass($concrete);
+
+
+        // 找到类的构造函数方法(初始化方法)
         $constructor = $reflector->getConstructor();
-        if(is_null($constructor)){
+
+
+        // 如果构造函数为空 直接返回实例
+        if (is_null($constructor)) {
             return $reflector->newInstance();
-        }else{
+        } else {
+            //如果不为空呢？
+            // 首先找到构造函数的构造参数
             $dependencies = $constructor->getParameters();
 
+
+            // 处理构造参数  返回构造参数的实例
             $instances = $this->dependencies($dependencies);
 
             return $reflector->newInstanceArgs($instances);
-
         }
 
     }
@@ -79,22 +90,26 @@ class Ioc
     public function dependencies($parameters)
     {
         $dependencies = [];
-        foreach($parameters as $parameter)
-        {
+        foreach($parameters as $parameter){
             $dependencies[] = $this->make($parameter->getClass()->name);
         }
         return $dependencies;
     }
 
+
 }
 
 $ioc = new Ioc();
+
 $ioc->bind('Log','DatabaseLog');
+
 $ioc->bind('user','User');
 
 $user = $ioc->make('user');
 
 $user->login();
+
+
 
 
 class Foo
